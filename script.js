d67a918ed1e4609ce07002507661dc09ecf8f71f
abeyakilesh <abeyakileshm@gmail.com>
2026-02-05 14:38:54 +0530

    Notes Half Done

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.Notes-container');
+const btn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+btn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+})
